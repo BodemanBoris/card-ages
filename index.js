068976a66ab12e04bdf5d29453d013b21fae1e20
BodemanBoris <borisbodemand@hotmail.com>
2024-03-01 17:53:52 +0100

    Return back styles

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const d = document,
-  $btn = d.querySelector(".leftBtn"),
-  $imgContainer = d.querySelector(".img__container");
 
-$btn.addEventListener("click", () => {
-  $imgContainer.classList.add("fadeOutLeft");
-  $imgContainer.addEventListener("animationend", () => {
-    $imgContainer.remove();
-  });
-});
