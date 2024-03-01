4be683c1a4cf1742c1e57cfa06bde42fbbe2aa5b
BodemanBoris <borisbodemand@hotmail.com>
2024-03-01 15:58:17 +0100

    retocando moveevent

--- index.js
+++ index.js
@@ -1,73 +1,102 @@
 let d = document,
   isAnimating = false,
   pullDeltaX = 0,
-  DECITION_THRESHOLD = 95;
+  DECISION_THRESHOLD = 95;
 
-const startDrag = (event) => {
+function startDrag(event) {
   if (isAnimating) return;
+
+  // get the first article element
+  const actualCard = event.target.closest("#agusImg");
+  if (!actualCard) return;
   event.preventDefault();
 
-  const $agusImg = event.target.closest("#agusImg");
-
-  if ($agusImg === null) return;
-
+  // get initial position of mouse or finger
   const startX = event.pageX ?? event.touches[0].pageX;
 
-  d.addEventListener("mousemove", onMove);
-  d.addEventListener("mouseup", onEnd);
+  // listen the mouse and touch movements
+  document.addEventListener("mousemove", onMove);
+  document.addEventListener("mouseup", onEnd);
 
-  d.addEventListener("touchmove", onMove, { passive: true });
-  d.addEventListener("touchend", onEnd, { passive: true });
+  document.addEventListener("touchmove", onMove, { passive: true });
+  document.addEventListener("touchend", onEnd, { passive: true });
 
   function onMove(event) {
+    // current position of mouse or finger
     const currentX = event.pageX ?? event.touches[0].pageX;
+
+    // the distance between the initial and current position
     pullDeltaX = currentX - startX;
 
+    // there is no distance traveled in X axis
     if (pullDeltaX === 0) return;
 
+    // change the flag to indicate we are animating
     isAnimating = true;
-    const deg = pullDeltaX / 10;
 
-    $agusImg.style.transform = `translateX(${pullDeltaX}px) rotate(${deg}deg)`;
-    $agusImg.style.cursor = "grabbing";
+    // calculate the rotation of the card using the distance
+    const deg = pullDeltaX / 14;
+
+    // apply the transformation to the card
+    actualCard.style.transform = `translateX(${pullDeltaX}px) rotate(${deg}deg)`;
+
+    // change the cursor to grabbing
+    actualCard.style.cursor = "grabbing";
+
+    // change opacity of the choice info
+    const opacity = Math.abs(pullDeltaX) / 100;
+    const isRight = pullDeltaX > 0;
+
+    /* const choiceEl = isRight
+      ? actualCard.querySelector(".choice.like")
+      : actualCard.querySelector(".choice.nope");
+
+    choiceEl.style.opacity = opacity;
+  } */
   }
 
   function onEnd(event) {
-    d.removeEventListener("mousemove", onMove);
-    d.removeEventListener("mousup", onEnd);
+    // remove the event listeners
+    document.removeEventListener("mousemove", onMove);
+    document.removeEventListener("mouseup", onEnd);
 
-    d.removeEventListener("touchmove", onMove, { passive: true });
-    d.removeEventListener("touchend", onEnd, { passive: true });
+    document.removeEventListener("touchmove", onMove);
+    document.removeEventListener("touchend", onEnd);
 
-    const decitionMade = Math.abs(pullDeltaX) >= DECITION_THRESHOLD;
+    // saber si el usuario tomo una decisión
+    const decisionMade = Math.abs(pullDeltaX) >= DECISION_THRESHOLD;
 
-    if (decitionMade) {
-      if (pullDeltaX > 0) {
-        $agusImg.classList.add("go-right");
-      } else {
-        $agusImg.classList.add("go-left");
-      }
-      $agusImg.addEventListener("transitionend", () => {
-        $agusImg.remove();
-        $agusImg.removeAttribute("style");
-        $agusImg.classList.remove("reset");
+    if (decisionMade) {
+      const goRight = pullDeltaX >= 0;
+
+      // add class according to the decision
+      actualCard.classList.add(goRight ? "go-right" : "go-left");
+      actualCard.addEventListener("transitionend", () => {
+        actualCard.remove();
       });
     } else {
-      $agusImg.classList.add("reset");
-      $agusImg.classList.remove("go-left", "go-right");
+      actualCard.classList.add("reset");
+      actualCard.classList.remove("go-right", "go-left");
+
+      actualCard.querySelectorAll(".choice").forEach((choice) => {
+        choice.style.opacity = 0;
+      });
     }
 
-    $agusImg.addEventListener("transitionend", () => {
-      $agusImg.removeAttribute("style");
-      $agusImg.classList.remove("reset");
+    // reset the variables
+    actualCard.addEventListener("transitionend", () => {
+      actualCard.removeAttribute("style");
+      actualCard.classList.remove("reset");
 
-      /* $like.removeAttribute("style");
-      $nope.removeAttribute("style"); */
       pullDeltaX = 0;
       isAnimating = false;
     });
-  }
-};
 
+    // reset the choice info opacity
+    actualCard
+      .querySelectorAll(".choice")
+      .forEach((el) => (el.style.opacity = 0));
+  }
+}
 d.addEventListener("mousedown", startDrag);
 d.addEventListener("touchStart", startDrag, { passive: true });
